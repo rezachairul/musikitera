07a4595751b6f9863cd19f1be10aee839c62cca7
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-09-10 10:31:42 +0700

    `Update footer component with social media icons and attribution information`

--- resources/views/components/public/footer.blade.php
+++ resources/views/components/public/footer.blade.php
@@ -15,22 +15,40 @@
             <p class="text-sm">Jl. Terusan Ryacudu, Way Huwi<br> Lampung Selatan</p>
             <p class="text-sm mt-1">Email: musikitera@gmail.com</p>
         </div>
-
+        
         {{-- Sosial Media --}}
         <div class="flex justify-center md:justify-start space-x-4">
-            <a href="https://instagram.com/musikitera" class="hover:text-yellow-500">
-                <i class="fab fa-instagram fa-lg"></i>
+            <a href="https://instagram.com/musikitera" class="hover:text-yellow-500" aria-label="Instagram">
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M7.5 2A5.5 5.5 0 002 7.5v9A5.5 5.5 0 007.5 22h9a5.5 5.5 0 005.5-5.5v-9A5.5 5.5 0 0016.5 2h-9zm0 2h9A3.5 3.5 0 0120 7.5v9a3.5 3.5 0 01-3.5 3.5h-9A3.5 3.5 0 014 16.5v-9A3.5 3.5 0 017.5 4zm9.75 1.25a.75.75 0 100 1.5.75.75 0 000-1.5zM12 7a5 5 0 100 10 5 5 0 000-10zm0 2a3 3 0 110 6 3 3 0 010-6z"/>
+                </svg>
             </a>
-            <a href="https://www.youtube.com/@musikitera5519" class="hover:text-yellow-500">
-                <i class="fab fa-youtube fa-lg"></i>
+            <a href="https://www.youtube.com/@musikitera5519" class="hover:text-yellow-500" aria-label="YouTube">
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M21.8 8s-.2-1.5-.8-2.2a3 3 0 00-2.2-.8C16.8 5 12 5 12 5s-4.8 0-6.8.2c-.8.1-1.6.3-2.2.8-.6.7-.8 2.2-.8 2.2S2 9.6 2 11.3v1.4c0 1.7.2 3.3.2 3.3s.2 1.5.8 2.2c.6.5 1.4.7 2.2.8C7.2 19 12 19 12 19s4.8 0 6.8-.2c.8-.1 1.6-.3 2.2-.8.6-.7.8-2.2.8-2.2s.2-1.6.2-3.3v-1.4c0-1.7-.2-3.3-.2-3.3zM10 9.8l5.2 3.2-5.2 3.2V9.8z"/>
+                </svg>
             </a>
-            <a href="#" class="hover:text-yellow-500">
-                <i class="fab fa-facebook fa-lg"></i>
+            <a href="#" class="hover:text-yellow-500" aria-label="Facebook">
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M22 12a10 10 0 10-11.6 9.9v-7h-2v-3h2v-2.3c0-2 1.2-3.2 3-3.2.9 0 1.8.1 1.8.1v2h-1c-1 0-1.3.6-1.3 1.2V12h2.2l-.4 3h-1.8v7A10 10 0 0022 12z"/>
+                </svg>
             </a>
         </div>
     </div>
 
-    <div class="border-t border-gray-700 mt-8 pt-4 text-center text-sm text-gray-500">
+    <div class=" border-gray-700 mt-8 pt-4 text-center text-sm text-gray-500">
         &copy; {{ date('Y') }} UKMBSM ITERA. All rights reserved.
+
+        <div class="mt-2">
+            Proyek ini dikembangkan oleh:
+            <br>
+            🐍 <a href="https://cobradev.vercel.app/" target="_blank" rel="noopener noreferrer" class="text-gray-400 hover:text-yellow-500 font-medium">
+                CobraDev
+            </a>
+            &nbsp;|&nbsp;
+            🌐 <a href="https://sigawariweb.netlify.app/" target="_blank" rel="noopener noreferrer" class="text-gray-400 hover:text-yellow-500 font-medium">
+                Sigawari
+            </a>
+        </div>
     </div>
 </footer>
