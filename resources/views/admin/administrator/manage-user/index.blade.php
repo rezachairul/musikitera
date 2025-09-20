00e5cae3c85fdf5ace0d9f87f36ec564b1b84b09
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-09-20 20:50:11 +0700

    `Update export button color in manage-user index view from green to amber`

--- resources/views/admin/administrator/manage-user/index.blade.php
+++ resources/views/admin/administrator/manage-user/index.blade.php
@@ -71,7 +71,7 @@
                 </button>
     
                 <!-- Export -->
-                 <a href="#" class="w-full sm:w-auto bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition-colors duration-200 flex items-center justify-center">
+                 <a href="#" class="w-full sm:w-auto bg-amber-600 text-white px-4 py-2 rounded-lg hover:bg-amber-700 transition-colors duration-200 flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 mr-2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25" />
                     </svg>
