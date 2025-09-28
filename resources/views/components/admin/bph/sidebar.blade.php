bb4fc46b19137541aab4654e7eeba2dc39b27572
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-09-28 21:29:30 +0700

    `Update route names in sidebar component to use 'manage-' prefix`

--- resources/views/components/admin/bph/sidebar.blade.php
+++ resources/views/components/admin/bph/sidebar.blade.php
@@ -64,7 +64,7 @@
         
         <!-- Manajemen Konten -->
         <div>
-            <button @click="openMenu = (openMenu === 'konten' ? null : 'konten')" class="w-full flex items-center justify-between px-3 py-2 rounded-md transition-colors {{ request()->routeIs('manage-hero.*') || request()->routeIs('profil.*') || request()->routeIs('layanan.*') || request()->routeIs('statistik.*') || request()->routeIs('galeri.*') || request()->routeIs('highlight.*') || request()->routeIs('testimoni.*') || request()->routeIs('cta-oprec.*') ? 'bg-gray-100 text-amber-600 font-medium' : 'text-gray-700 hover:bg-gray-100 hover:text-amber-600' }}">
+            <button @click="openMenu = (openMenu === 'konten' ? null : 'konten')" class="w-full flex items-center justify-between px-3 py-2 rounded-md transition-colors {{ request()->routeIs('manage-hero.*') || request()->routeIs('manage-profile.*') || request()->routeIs('manage-layanan.*') || request()->routeIs('manage-statistik.*') || request()->routeIs('manage-galeri.*') || request()->routeIs('manage-highlight.*') || request()->routeIs('manage-testimoni.*') || request()->routeIs('manage-cta.*') ? 'bg-gray-100 text-amber-600 font-medium' : 'text-gray-700 hover:bg-gray-100 hover:text-amber-600' }}">
                 
                 <div class="flex items-center gap-3">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -85,37 +85,37 @@
                 </a>
 
                 <a href="{{ route('manage-profile.index') }}"
-                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('profil.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('manage-profile.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     Profil Organisasi
                 </a>
 
                 <a href="{{ route('manage-layanan.index') }}"
-                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('layanan.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('manage-layanan.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     Layanan
                 </a>
 
                 <a href="{{ route('manage-statistik.index') }}"
-                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('statistik.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('manage-statistik.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     Statistik Publik
                 </a>
 
                 <a href="{{ route('manage-galeri.index') }}"
-                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('galeri.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('manage-galeri.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     Galeri Kegiatan
                 </a>
 
                 <a href="{{ route('manage-highlight.index') }}"
-                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('highlight.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('manage-highlight.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     Highlight Kegiatan
                 </a>
 
                 <a href="{{ route('manage-testimoni.index') }}"
-                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('testimoni.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('manage-testimoni.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     Apa Kata Mereka
                 </a>
 
                 <a href="{{ route('manage-cta.index') }}"
-                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('cta-oprec.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                    class="px-3 py-1 rounded-md transition-colors {{ request()->routeIs('manage-cta.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     CTA Oprec
                 </a>
             </div>
@@ -123,7 +123,7 @@
         
         <!-- Publikasi dan Informasi -->
         <div>
-            <button @click="openMenu = (openMenu === 'publikasi' ? null : 'publikasi')" class="w-full flex items-center justify-between px-3 py-2 rounded-md transition-colors {{ request()->routeIs('dokumen.*') || request()->routeIs('kegiatan.*') || request()->routeIs('pengumuman.*')  ? 'bg-gray-100 text-amber-600 font-medium'  : 'text-gray-700 hover:bg-gray-100 hover:text-amber-600' }}">
+            <button @click="openMenu = (openMenu === 'publikasi' ? null : 'publikasi')" class="w-full flex items-center justify-between px-3 py-2 rounded-md transition-colors {{ request()->routeIs('manage-dokumen.*') || request()->routeIs('manage-kegiatan.*') || request()->routeIs('manage-pengumuman.*')  ? 'bg-gray-100 text-amber-600 font-medium'  : 'text-gray-700 hover:bg-gray-100 hover:text-amber-600' }}">
                 <div class="flex items-center gap-3">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46" />
@@ -138,13 +138,13 @@
 
             <div x-show="openMenu === 'publikasi'" x-cloak 
                 class="ml-10 flex flex-col gap-1 mt-1">                
-                <a href="{{ route('manage-dokumen.index') }}" class="px-3 py-1 rounded-md transition-colors  {{ request()->routeIs('dokumen.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                <a href="{{ route('manage-dokumen.index') }}" class="px-3 py-1 rounded-md transition-colors  {{ request()->routeIs('manage-dokumen.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     Dokumen
                 </a>
-                <a href="{{ route('manage-kegiatan.index') }}" class="px-3 py-1 rounded-md transition-colors  {{ request()->routeIs('kegiatan.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                <a href="{{ route('manage-kegiatan.index') }}" class="px-3 py-1 rounded-md transition-colors  {{ request()->routeIs('manage-kegiatan.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     Kegiatan
                 </a>
-                <a href="{{ route('manage-pengumuman.index') }}" class="px-3 py-1 rounded-md transition-colors  {{ request()->routeIs('pengumuman.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
+                <a href="{{ route('manage-pengumuman.index') }}" class="px-3 py-1 rounded-md transition-colors  {{ request()->routeIs('manage-pengumuman.*') ? 'text-amber-600 font-medium bg-gray-100' : 'text-gray-600 hover:text-amber-600' }}">
                     Pengumuman Penting
                 </a>
             </div>
