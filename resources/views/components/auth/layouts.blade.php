802a10222bc6b972a82737c1d3a2a0b927b114bc
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-10-09 10:48:24 +0700

    `Update auth layouts view to include Vite and remove Tailwind CDN`

--- resources/views/components/auth/layouts.blade.php
+++ resources/views/components/auth/layouts.blade.php
@@ -4,9 +4,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>MusikITERA | {{ $title }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         
         <link id="favicon" rel="shortcut icon" href="{{ asset('assets/img/favicon/favicon.ico') }}" type="image/x-icon">
-        <script src="https://cdn.tailwindcss.com"></script>
+        
     </head>
     <body class="bg-gray-100 flex items-center justify-center h-screen">
         {{ $slot }}
